0614e284129d4ec7fd2c733ad4156c2d61533ef1
Nick Vincent-Maloney <nicorellius@gmail.com>
2016-06-07 19:57:02 -0700

    deactivate https/tls and fix static and media directories.

--- pdxpixel/conf/settings/production.py
+++ pdxpixel/conf/settings/production.py
@@ -6,13 +6,13 @@ DEBUG = False
 with open('/etc/prv/pdxpixel/secret_key.txt') as secret_key:
     SECRET_KEY = secret_key.read().strip()
 
-STATIC_ROOT = str(SITE_DIR.path('static'))
-MEDIA_ROOT = str(SITE_DIR.path('media'))
+STATIC_ROOT = '/var/www/pdxpixel/static'
+MEDIA_ROOT = '/var/www/pdxpixel/media'
 
 ALLOWED_HOSTS = ['pdxpixel.com', 'www.pdxpixel.com']
 
-SECURE_SSL_REDIRECT = True
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_HSTS_SECONDS = 3600
+# SECURE_SSL_REDIRECT = True
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+# SECURE_HSTS_SECONDS = 3600
